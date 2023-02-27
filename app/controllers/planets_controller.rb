f5a5e804113779a1c1580d1d236587268d35ee79
Lisa Millet <lisamilletpro@gmail.com>
2023-02-27 18:03:43 +0100

    planets-index-controller+view

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,47 @@
+class PlanetsController < ApplicationController
+  def index
+    @planets = Planet.all
+  end
+
+  # def show
+  #   @planet = Planet.find(params[:id])
+  # end
+
+  # def new
+  #   @planet = Planet.new
+  # end
+
+  # def create
+  #   @planet = Planet.new(planet_params)
+  #   @planet.user = @current_user
+  #   raise
+  #   if @planet.save
+  #     redirect_to root_path
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+#   def edit
+#     @planet = Planet.find(params[:id])
+#   end
+
+#   def update
+#     @planet = Planet.find(params[:id])
+#     @planet.update(planet_params)
+#     redirect_to planet_path(@planet)
+#   end
+
+#   def destroy
+#     @planet = Planet.find(params[:id])
+#     @planet.destroy
+#     redirect_to planets_path, status: :see_other
+#   end
+
+#   private
+
+# def planet_params
+#   params.require(:planet).permit(:name, :location, :user_id)
+# end
+
+end
