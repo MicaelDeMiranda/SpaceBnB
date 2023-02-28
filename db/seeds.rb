727243f2cd1aba6eadcddf4138c025bb543d56de
Micael De Miranda <demirandamicael@gmail.com>
2023-02-28 17:36:10 +0100

    finished the seed and everything works

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,24 +14,24 @@ user = User.create(email: "toto@toto.com", password: "mdpmdp")
 planets = [
   { name: 'Arrakis', location: "500 années-lumière", price_per_night: 50, user: user },
   { name: 'Cérès', location: "413 millions km", capacity: "10", price_per_night: "300", user: user },
-  { name: "Éris", location: "565 millions km", capacity: "500", price_per_night: "250" },
-  { name: "Hauméa", location: "5,2 milliards km", planet_photo: "Hauméa.jpeg", capacity: "230", price_per_night: "270" },
-  { name: "Jupiter", location: "778 millions km", planet_photo: "Jupiter.jpeg", capacity: "10000", price_per_night: "700" },
-  { name: "Krypton", location: "27 années-lumière", planet_photo: "Krypton.jpeg", capacity: "1", price_per_night: "1000000" },
-  { name: "Lune", location: "150 millions km", planet_photo: "Lune.jpeg", capacity: "2", price_per_night: "1000000" },
-  { name: "Makémaké", location: "7,8 milliards km", planet_photo: "Makémaké.jpeg", capacity: "50", price_per_night: "500" },
-  { name: "Mars", location: "227 millions km", planet_photo: "Mars.jpeg", capacity: "50000", price_per_night: "1000" },
-  { name: "Mercure", location: "58 millions km", planet_photo: "Mercure.png", capacity: "56", price_per_night: "650"},
-  { name: "Neptune", location: "4,5 milliards km", planet_photo: "Neptune.jpeg", capacity: "25", price_per_night: "75" },
-  { name: "Pandora", location: "4,4 années-lumière", planet_photo: "Pandora.jpeg", capacity: "22530", price_per_night: "1000000" },
-  { name: "Pluton", location: "5,9 milliards km", planet_photo: "Pluton.jpeg", capacity: "5", price_per_night: "15000" },
-  { name: "Saturne", location: "1,5 milliards km", planet_photo: "Saturne.jpeg", capacity: "100000", price_per_night: "80000" },
-  { name: "Soleil", location: "0 km", planet_photo: "Soleil.jpeg", capacity: "100", price_per_night: "60000" },
-  { name: "Tatooine", location: "108 années-lumière", planet_photo: "Tatooine.webp", capacity: "3000", price_per_night: "75000" },
-  { name: "Terre", location: "150 millions km", planet_photo: "Terre.png", capacity: "100000", price_per_night: "10" },
-  { name: "Uranus", location: "2,8 millions km", planet_photo: "Uranus.jpeg", capacity: "2000", price_per_night: "60000" },
-  { name: "Vénus", location: "108 milliards km", planet_photo: "Vénus.jpeg", capacity: "6", price_per_night: "90000" },
-  { name: "Vulcain", location: "653 années-lumière", planet_photo: "Vulcain.webp", capacity: "7000", price_per_night: "150000" }
+  { name: "Éris", location: "565 millions km", capacity: "500", price_per_night: "250", user: user },
+  { name: "Hauméa", location: "5,2 milliards km", capacity: "230", price_per_night: "270", user: user },
+  { name: "Jupiter", location: "778 millions km", capacity: "10000", price_per_night: "700", user: user },
+  { name: "Krypton", location: "27 années-lumière", capacity: "1", price_per_night: "1000000", user: user },
+  { name: "Lune", location: "150 millions km", capacity: "2", price_per_night: "1000000", user: user },
+  { name: "Makémaké", location: "7,8 milliards km", capacity: "50", price_per_night: "500", user: user },
+  { name: "Mars", location: "227 millions km", capacity: "50000", price_per_night: "1000", user: user },
+  { name: "Mercure", location: "58 millions km", capacity: "56", price_per_night: "650", user: user},
+  { name: "Neptune", location: "4,5 milliards km", capacity: "25", price_per_night: "75", user: user },
+  { name: "Pandora", location: "4,4 années-lumière", capacity: "22530", price_per_night: "1000000", user: user },
+  { name: "Pluton", location: "5,9 milliards km", capacity: "5", price_per_night: "15000", user: user },
+  { name: "Saturne", location: "1,5 milliards km", capacity: "100000", price_per_night: "80000", user: user },
+  { name: "Soleil", location: "0 km", capacity: "100", price_per_night: "60000", user: user },
+  { name: "Tatooine", location: "108 années-lumière", capacity: "3000", price_per_night: "75000", user: user },
+  { name: "Terre", location: "150 millions km", capacity: "100000", price_per_night: "10", user: user },
+  { name: "Uranus", location: "2,8 millions km", capacity: "2000", price_per_night: "60000", user: user },
+  { name: "Vénus", location: "108 milliards km", capacity: "6", price_per_night: "90000", user: user },
+  { name: "Vulcain", location: "653 années-lumière", capacity: "7000", price_per_night: "150000", user: user }
 ]
 
 planets.each do |data|
@@ -45,7 +45,7 @@ end
 puts "Finished!"
 
 # arrakis = { name: "Arrakis", location: "500 années-lumière", price_per_night: 50, user: user }
-# ceres = { name: "Cérès", location: "413 millions km", planet_photo: "Cérès.jpeg", capacity: "10", price_per_night: "300" }
+# ceres = { name: "Cérès", location: "413 millions km",  "Cérès.jpeg", capacity: "10", price_per_night: "300" }
 # eris = { name: "Éris", location: "565 millions km", planet_photo: "Eris.jpeg", capacity: "500", price_per_night: "250" }
 # haumea = { name: "Hauméa", location: "5,2 milliards km", planet_photo: "Hauméa.jpeg", capacity: "230", price_per_night: "270" }
 # jupiter = { name: "Jupiter", location: "778 millions km", planet_photo: "Jupiter.jpeg", capacity: "10000", price_per_night: "700" }
