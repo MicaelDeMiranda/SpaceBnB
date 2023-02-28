9752a73691407491979bc4be7e84572198c1fa09
muriel bourre <muriel.bourre.18@gmail.com>
2023-02-28 15:13:22 +0100

    wip

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -21,15 +21,18 @@ class PlanetsController < ApplicationController
 
   def update
     @planet = Planet.find(params[:id])
-    @planet.update(params[:planet])
-    # redirect_to planet_path(@planet)
     authorize @planet
+    @planet.update(planet_params)
+    redirect_to planet_path(@planet)
+
   end
 
   def destroy
     @planet = Planet.find(params[:id])
+    authorize @planet
     @planet.destroy
-    # redirect_to planet_path, status: :see_other
+
+    redirect_to root_path, status: :see_other
   end
 
   def create
