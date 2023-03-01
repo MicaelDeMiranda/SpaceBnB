e2227fe5ad4505ce495166c5c9f891f37e7fa3f2
Lisa Millet <lisamilletpro@gmail.com>
2023-03-01 14:09:51 +0100

    fixing navbar + affichage nul quand 0 réservation

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,5 +1,5 @@
 class PlanetsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new]
 
   def index
     @planets = Planet.all
